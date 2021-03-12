669e75bfd8a931600023a3c1959dd18387f393e8
Matt Dolan <mdolan.code@gmail.com>
2021-03-12 13:09:42 -0800

    Initial Commit

---
+++ Matt Dolan's Card/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Matt Dolan's Card
+//
+//  Created by Matt Dolan External macOS on 2021-03-12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Matt Dolan's Card/Matt_Dolan_s_CardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Matt_Dolan_s_CardApp.swift
+//  Matt Dolan's Card
+//
+//  Created by Matt Dolan External macOS on 2021-03-12.
+//
+
+import SwiftUI
+
+@main
+struct Matt_Dolan_s_CardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
